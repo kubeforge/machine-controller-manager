d966000a640ec7efc4b1aaabaace2e6e949d2d3c
i331134 <hardik.dodiya@sap.com>
2018-11-20 02:24:20 +0530

    Integrate grpc based client with drivers

---
+++ pkg/driver/cmi-client.go
@@ -0,0 +1,134 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package driver
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"time"
+
+	v1alpha1 "github.com/gardener/machine-controller-manager/pkg/apis/machine/v1alpha1"
+	cmipb "github.com/gardener/machine-spec/lib/go/cmi"
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/golang/glog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type cmiClient interface {
+	// CreateMachine(ctx context.Context, req *cmipb.CreateMachineRequest) (*cmipb.CreateMachineResponse, error)
+	// DeleteMachine(ctx context.Context, req *cmipb.DeleteMachineRequest) (*cmipb.DeleteMachineResponse, error)
+	// ListMachines(ctx context.Context, req *cmipb.ListMachinesRequest) (*cmipb.ListMachinesResponse, error)
+
+	CreateMachine(ctx context.Context) (string, string, error)
+	DeleteMachine(ctx context.Context) error
+	ListMachines(ctx context.Context) (string, error)
+}
+
+type cmiDriverClient struct {
+	driverName           string
+	machineClientCreator machineClientCreator
+	machineClass         *v1alpha1.AWSMachineClass
+	CloudConfig          *corev1.Secret
+	UserData             string
+	MachineID            string
+	MachineName          string
+}
+
+type machineClientCreator func(driverName string) (
+	machineClient cmipb.MachineClient,
+	closer io.Closer,
+	err error,
+)
+
+func newMachineClient(driverName string) (machineClient cmipb.MachineClient, closer io.Closer, err error) {
+	var conn *grpc.ClientConn
+	conn, err = newGrpcConn(driverName)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	machineClient = cmipb.NewMachineClient(conn)
+	return machineClient, conn, nil
+}
+
+// NewCmiDriverClient
+func NewCmiDriverClient(machineID, driverName string, secret *corev1.Secret, machineClass interface{}, machineName string) *cmiDriverClient {
+	c := &cmiDriverClient{
+		driverName:           driverName,
+		machineClientCreator: newMachineClient,
+		machineClass:         machineClass.(*v1alpha1.AWSMachineClass),
+		CloudConfig:          secret,
+		UserData:             string(secret.Data["userData"]),
+		MachineID:            machineID,
+		MachineName:          machineName,
+	}
+	return c
+}
+
+func (c *cmiDriverClient) CreateMachine(ctx context.Context) (string, string, error) {
+	glog.V(4).Info("Calling CreateMachine rpc")
+
+	machineClient, closer, err := c.machineClientCreator(c.driverName)
+	if err != nil {
+		return "", "", err
+	}
+	defer closer.Close()
+
+	req := &cmipb.CreateMachineRequest{
+		Providerconfig: "MachineClass.ProviderConfig here",
+	}
+	resp, err := machineClient.CreateMachine(ctx, req)
+	if err != nil {
+		return "", "", err
+	}
+	fmt.Println(resp)
+
+	return "", "", status.Error(codes.Unimplemented, "")
+}
+
+func (c *cmiDriverClient) DeleteMachine(ctx context.Context, req *cmipb.DeleteMachineRequest) (*cmipb.DeleteMachineResponse, error) {
+	glog.V(4).Info("Calling DeleteMachine rpc")
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func (c *cmiDriverClient) ListMachines(ctx context.Context, req *cmipb.ListMachinesRequest) (*cmipb.ListMachinesResponse, error) {
+	glog.V(4).Info("Calling ListMachine rpc")
+	return nil, status.Error(codes.Unimplemented, "")
+}
+
+func newGrpcConn(driverName string) (*grpc.ClientConn, error) {
+	if driverName == "" {
+		return nil, fmt.Errorf("driver name is empty")
+	}
+
+	network := "tcp"
+	addr := "127.0.0.1:8080"
+	glog.V(4).Infof("creating new gRPC connection for [%s://%s]", network, addr)
+
+	return grpc.Dial(
+		addr,
+		grpc.WithInsecure(),
+		grpc.WithDialer(func(target string, timeout time.Duration) (net.Conn, error) {
+			return net.Dial(network, target)
+		}),
+	)
+}
